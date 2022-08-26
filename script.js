bc664c84b4f254f53e9698507c3067c47e4ce473
tygrski <77369211+tygrski@users.noreply.github.com>
2022-08-26 12:31:52 -0500

    multiple player function, and scores add

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
 'use strict';
 
-// Selecting elements
+// Selecting elements: Player 1 is index 0(--0), Player 2 is index 1(--1)
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const current0El = document.getElementById("current--0");
-const current1El = document.getElementById("current--1");
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnHold = document.querySelector('.btn--hold');
 const btnRoll = document.querySelector('.btn--roll');
 
-
-// Starting condition 
+// Starting condition
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-let curentScore = 0;
-
-
+// for array of players , index of player 1 = 0, player2 (num is value of score, idexe #'s are the players);
+// total scores
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling Dice Functionality
-btnRoll.addEventListener('click', function(){
-    // 1. Generate random dice roll
-    const dice =  Math.trunc(Math.random() * 6) + 1;
-    console.log(dice);
-    
-    // 2. Display Dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`
+btnRoll.addEventListener('click', function () {
+  // 1. Generate random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
 
-    // 3. Check for roll of 1, if true ,
-    if(dice !== 1){
-        // add dice to current score
-        curentScore += dice;
-        current0El.textContent = curentScore; //change later
+  // 2. Display Dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
 
-    } else {
-     // switch player
-     
-    }
-
-})
-
+  // 3. Check for roll of 1, if true ...
+  if (dice !== 1) {
+    // add dice to current score
+    currentScore += dice;
+    // dynamicly select the element based which person is playing
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    // if not true
+  } else {
+    // rest the score text of player who just finished
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    //   rest value for next player
+    currentScore = 0;
+    // switch player( 0 or 1 index) - terany operator
+    activePlayer = activePlayer === 0 ? 1 : 0;
+  }
+});
